adca55ea41922da933e765c3549d66ec2bead7e0
Catie Jo <catiejo@caties-mbp.zhaw.ch>
2018-10-26 10:20:24 +0200

    I'll receive an email wtih every attempt from participants

---
+++ Assets/Scripts/EmailSender.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+public class EmailSender
+{
+
+    public static void SendEmailWithAttempt(string attempt)
+    {
+        MailMessage mail = new MailMessage();
+
+        mail.From = new MailAddress("zhaw.eva2@gmail.com");
+        mail.To.Add("pide@zhaw.ch");
+        mail.Subject = "Test Mail";
+        mail.Body = attempt;
+
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new System.Net.NetworkCredential("zhaw.eva2@gmail.com", "T6hX$}i7}GNHA]2G99.7>DjZ4Pb&KV(P2*x%RW27T") as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors)
+            { return true; };
+        smtpServer.Send(mail);
+        Debug.Log("success");
+    }
+}
